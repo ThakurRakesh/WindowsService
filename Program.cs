465109a61315ef15f73a4234d6b6cfb7557b3f84
Rakesh Kumar <rakkarajput@gmail.com>
2023-12-10 12:11:22 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rakesh_Kumar_HelloWorldWindowsService
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Rakesh_Kumar_HelloWorldWindowsService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ Rakesh_Kumar_HelloWorldWindowsService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace Rakesh_Kumar_HelloWorldWindowsService
+{
+    public partial class Rakesh_Kumar_HelloWorldWindowsService : ServiceBase
+    {
+        private Timer timer;
+
+        public Rakesh_Kumar_HelloWorldWindowsService()
+        {
+        }
+
+         protected override void OnStart(string[] args)
+        {
+            // Set up a timer to call the TimerElapsed method every 10 seconds
+            timer = new Timer
+            {
+                Interval = TimeSpan.FromSeconds(10).TotalMilliseconds
+            };
+            timer.Elapsed += TimerElapsed;
+            timer.Start();
+        }
+
+        protected override void OnStop()
+        {
+            // Stop the timer when the service is stopped
+            timer.Stop();
+            timer.Dispose();
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            // Write "Hello, World!" to the console
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
